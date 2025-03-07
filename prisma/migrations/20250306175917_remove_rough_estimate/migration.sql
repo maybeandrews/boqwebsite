da7b6ba0c1829d7284f904a7bbfa36c712a09680
Andrews <andymanny12@gmail.com>
2025-03-07 13:35:00 +0530

    Updated schema

---
+++ prisma/migrations/20250306175917_remove_rough_estimate/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `roughEstimate` on the `BOQ` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BOQ" DROP COLUMN "roughEstimate";
