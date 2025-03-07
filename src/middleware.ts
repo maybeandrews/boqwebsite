f575666aba9d3c09e289201fa7de6fd70844e54c
Andrews <andymanny12@gmail.com>
2025-03-07 18:53:02 +0530

    commented out middleware for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 // middleware.ts
+/*
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
@@ -76,3 +77,4 @@ export const config = {
         "/login",
     ],
 };
+*/
